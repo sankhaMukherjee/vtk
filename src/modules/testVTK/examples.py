7965418352cbdb7c22516fabc11029da69eb7062
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2020-01-10 17:06:34 +0800

    now we have a voxel

--- src/modules/testVTK/examples.py
+++ src/modules/testVTK/examples.py
@@ -25,7 +25,70 @@ class MySphere():
         self.sphere.SetPhiResolution(resolution)
         return
 
-    
+class MyCube():
+
+    def __init__(self):
+
+        self.cube = vtk.vtkCubeSource()
+        self.cube.SetCenter(0,0,0)
+        self.cube.SetXLength(1)
+        self.cube.SetYLength(1)
+        self.cube.SetZLength(1)
+        
+        self.cubeMapper = vtk.vtkPolyDataMapper()
+        self.cubeMapper.SetInputConnection( self.cube.GetOutputPort() )
+
+        self.cubeActor = vtk.vtkActor()
+        self.cubeActor.SetMapper( self.cubeMapper )
+
+        return
+
+    def setSize(self, size=1):
+
+        self.cube.SetXLength(size)
+        self.cube.SetYLength(size)
+        self.cube.SetZLength(size)
+
+        return
+
+class MyVoxel():
+
+    def __init__(self):
+
+        pts = [
+            [0, 0, 0],
+            [1, 0, 0],
+            [0, 1, 0],
+            [1, 1, 0],
+            [0, 0, 1],
+            [1, 0, 1],
+            [0, 1, 1],
+            [1, 1, 1],
+        ]
+
+        self.points = vtk.vtkPoints()
+        self.voxel  = vtk.vtkVoxel()
+        self.mapper = vtk.vtkDataSetMapper()
+        self.actor  = vtk.vtkActor()
+
+
+        for i, p in enumerate(pts):
+            self.points.InsertNextPoint( *p )
+            self.voxel.GetPointIds().SetId(i, i)
+
+        self.ug = vtk.vtkUnstructuredGrid()
+        self.ug.SetPoints(self.points)
+        self.ug.InsertNextCell(self.voxel.GetCellType(), self.voxel.GetPointIds())
+
+        self.mapper.SetInputData(self.ug)
+        self.actor.SetMapper(self.mapper)
+
+        self.actor.GetProperty().SetColor(vtk.vtkNamedColors().GetColor3d("Tomato"))
+        self.actor.GetProperty().EdgeVisibilityOn()
+        self.actor.GetProperty().SetLineWidth(3)
+        self.actor.GetProperty().SetOpacity(.5)
+        return
+        
 
 
 def createSphere():
@@ -38,24 +101,29 @@ def createSphere():
     iren.SetRenderWindow(renWin)
 
     allSpheres = []
+    allVoxels = []
     for x in range(10):
         for y in range(10):
             for z in range(10):
 
                 sphere = MySphere()
                 sphere.sphere.SetCenter(x, y, z)
-                sphere.sphere.SetRadius(0.5)
+                sphere.sphere.SetRadius(0.1)
                 sphere.setColor([1,0,0])
                 sphere.setResolution(10)
 
                 allSpheres.append(sphere)
 
-
     for sphere in allSpheres:
         ren.AddActor(sphere.sphereActor)
+        
 
+    voxel = MyVoxel()
+    allVoxels.append( voxel )
+    for voxel in allVoxels:
+        ren.AddViewProp( voxel.actor )
 
-    renWin.SetSize(300, 300)
+    renWin.SetSize(1500, 1500)
     renWin.SetWindowName('Cylinder')
 
     iren.Initialize()
