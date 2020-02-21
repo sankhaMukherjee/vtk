f34b980403eff1f6d790e172ac47cb22e33d5e3e
sankha <sankha.mukherjee@gmail.com>
2020-02-21 23:47:47 +0800

    contour lines obtained from matplotlib

--- src/modules/contours/contours.py
+++ src/modules/contours/contours.py
@@ -1,5 +1,7 @@
 from logs import logDecorator as lD 
 import jsonref, pprint
+import matplotlib.pyplot as plt
+import numpy as np
 
 config = jsonref.load(open('../config/config.json'))
 logBase = config['logging']['logBase'] + '.modules.contours.contours'
@@ -17,7 +19,26 @@ def doSomething(logger):
         The logger used for logging error information
     '''
 
-    print('We are in contours')
+    x, y = np.meshgrid(np.linspace(-5, 5, 100), np.linspace(-5, 5, 100))
+    r = np.sqrt( x**2 + y**2 )
+    z = np.sinc( r )
+
+    cs = plt.contourf( x, y, z )
+
+    print(cs.levels)
+
+    # for i, segment in enumerate(cs.allsegs):
+    #     print(i, len(segment))
+    #     plt.figure()
+    #     for j, polygons in enumerate(segment):
+    #         print('\t', j, len(polygons))
+    #         polygons = np.array(polygons)
+    #         x1, y1 = polygons.T 
+    #         plt.plot(x1, y1)
+    #     plt.xlim([-5, 5])
+    #     plt.ylim([-5, 5])
+    
+    plt.show()
 
     return
 
@@ -42,8 +63,7 @@ def main(logger, resultsDict):
     print('='*30)
     print('Main function of contours')
     print('='*30)
-    print('We get a copy of the result dictionary over here ...')
-    pprint.pprint(resultsDict)
+
 
     doSomething()
 
