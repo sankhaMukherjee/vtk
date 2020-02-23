64295e37b8fca317f2c9d09803e1f9a55566f0ca
sankha <sankha.mukherjee@gmail.com>
2020-02-23 14:56:05 +0800

    started adding the gaussians

--- src/modules/contours/contours.py
+++ src/modules/contours/contours.py
@@ -12,6 +12,20 @@ from skimage import measure
 config = jsonref.load(open('../config/config.json'))
 logBase = config['logging']['logBase'] + '.modules.contours.contours'
 
+def createGaussian(xVals, yVals, zVals, mu, sigma=1.0, scale=1):
+
+    mu_x, mu_y, mu_z = mu
+
+    x, y, z = np.meshgrid(xVals, yVals, zVals)
+
+    result  = (x - mu_x)**2/(2*sigma**2)
+    result += (y - mu_y)**2/(2*sigma**2)
+    result += (z - mu_z)**2/(2*sigma**2)
+
+    result = scale * (1/np.sqrt(2*np.pi*(sigma**2))) * np.exp( - result )
+
+    return result
+
 
 def pyVistaLine():
 
@@ -27,18 +41,14 @@ def pyVistaLine():
 
     points = np.column_stack((dInt, l_xInt, l_yInt))
 
-
     poly = pv.PolyData()
     poly.points = points
     the_cell = np.arange(0, len(points), dtype=np.int)
     the_cell = np.insert(the_cell, 0, len(points))
     poly.lines = the_cell
     
-
     poly["scalars"] = np.ones( np.shape(cInt) )
     poly["cgi"] = cInt*1e-3
-
-
 
     planes = []
     for d in days:
@@ -58,96 +68,6 @@ def pyVistaLine():
         plt.add_mesh( plane,  color='white', opacity=0.1, show_edges=True, edge_color = 'black' )
     camPos = plt.show()
     print(camPos)
-
-    return
-
-@lD.log(logBase + '.doSomethingElse')
-def doSomethingElse(logger):
-
-
-    x, y = np.meshgrid(np.linspace(-5, 5, 100), np.linspace(-5, 5, 100))
-    r = np.sqrt( x**2 + y**2 )
-    z = np.sinc( r )
-
-    contours = measure.find_contours( z, 0.1 )
-    for c in contours:
-        x, y = c.T 
-        plt.plot(x, y, '+-')
-    # print(contours)
-    plt.show()
-
-    return
-
-@lD.log(logBase + '.doSomethingElse1')
-def doSomethingElse1(logger):
-
-
-    x, y, z = np.meshgrid(np.linspace(-5, 5, 20),
-                      np.linspace(-5, 5, 20),
-                      np.linspace(-5, 5, 5))
-
-    points = np.empty((x.size, 3))
-    points[:, 0] = x.ravel('F')
-    points[:, 1] = y.ravel('F')
-    points[:, 2] = z.ravel('F')
-
-    # Compute a direction for the vector field
-    direction = np.sin(points)**3
-
-    # plot using the plotting class
-    plobj = pv.Plotter()
-    plobj.add_arrows(points, direction, 0.5)
-    plobj.show()
-
-    return
-
-@lD.log(logBase + '.doSomething')
-def doSomething(logger):
-    '''print a line
-    
-    This function simply prints a single line
-    
-    Parameters
-    ----------
-    logger : {logging.Logger}
-        The logger used for logging error information
-    '''
-
-    x, y = np.meshgrid(np.linspace(-5, 5, 100), np.linspace(-5, 5, 100))
-    r = np.sqrt( x**2 + y**2 )
-    z = np.sinc( r )
-
-    cs = plt.contourf( x, y, z )
-    plt.close()
-    print(cs.levels)
-    
-    for i, segment in enumerate(cs.allsegs):
-        print(i, len(segment))
-        for j, polygon in enumerate(segment):
-            polygon = np.array(polygon)
-            # print(polygon.T.shape)
-            x1, y1 = polygon.T 
-            print(f' -> {j}, {len(polygon):4d}, ({x1[0]:.2f},{y1[0]:.2f}) -> ({x1[-1]:.2f},{y1[-1]:.2f}) ')
-    
-    
-    p1 = cs.allsegs[0][0] # seg 0, poly 0
-    p2 = cs.allsegs[1][0] # seg 1, poly 0
-    p3 = cs.allsegs[1][1] # seg 1, poly 1
-
-    print('+===========================================')
-    for p in p2:
-        print(f'| {p[0]:.6f}, {p[1]:.6f}')
-    print('+===========================================')
-
-    plt.figure()
-    for i, p in enumerate([p1.T, p2.T, p3.T]):
-        plt.plot( p[0], p[1], '+-', label=f'{i}' )
-        plt.plot( [p[0][0]], [p[1][0]],   'o', mfc='None',mec='k', ms=12)
-        plt.plot( [p[0][1]], [p[1][1]],   'o', mfc='None',mec='red', ms=8)
-        plt.plot( [p[0][-1]], [p[1][-1]], 's', mfc='None',mec='k', ms=12)
-        plt.plot( [p[0][-2]], [p[1][-2]], 's', mfc='None',mec='red', ms=8)
-    plt.legend()
-    plt.show()
 
     return
 
@@ -173,8 +93,37 @@ def main(logger, resultsDict):
     print('Main function of contours')
     print('='*30)
 
+    day = 7
+    xVals = day + np.linspace(-3, 3, 101)
+    yVals = np.linspace(-6, 6, 100)
+    zVals = np.linspace(-6, 6, 100)
 
-    pyVistaLine()
+    data = [
+        # [(day,  3,  3), 7],
+        # [(day, -3,  3), 6],
+        # [(day, -3,  3), 4],
+        [(day,  0,  0), 4],
+        [(day, -6, -6), 4],
+        [(day,  6,  6), 4],
+    ]
+
+    results = []
+    for i, (mu, scale) in enumerate(data):
+        temp = createGaussian(xVals, yVals, zVals, mu, scale=scale)
+        results.append(temp)
+        if i == 0:
+            result = temp
+        else:
+            result += temp
+
+
+    for r in results:
+        plt.figure()
+        plt.contourf( r[ 50 ,:,:] )
+        plt.figure()
+        plt.contourf( r[ :,50,:] )
+    plt.show()
+    # pyVistaLine()
 
     print('Getting out of Module 1')
     print('-'*30)
